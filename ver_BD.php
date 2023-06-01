345ae1e1307d5639fc0c971b2a94f49a57111d8d
wpfsilva <wpf.silva@outlook.com>
2023-06-01 14:02:51 -0300

    Update ver_BD.php

--- ver_BD.php
+++ ver_BD.php
@@ -63,8 +63,10 @@
   </footer>
   </main>
  <link rel="stylesheet" href="assets/css/style.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-LQCB2jz3UvVfqekEqZMnKd1cMT4ls+HeuSmzJfl2d3g43SlBCYBYqxU2zSsy+DoH" crossorigin="anonymous"></script>
-</body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-LQCB2jz3UvVfqekEqZMnKd1cMT4ls+HeuSmzJfl2d3g43SlBCYBYqxU2zSsy+DoH" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+  </body>
 
 </html>
